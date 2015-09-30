115f541565f67f6e065064c61b005f86e01cb676
mcgi5sr2 <mcgi5sr2@gmail.com>
2015-09-30 10:58:21 +0100

    i9300: Use Exynos4RIL from HW samsung repo Exynos4RIL is common on smdk4412 devices
    Change-Id: Iad6399f104095ac82cc9bb5aee5ee74a1c4cb013

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ endif
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6262
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/i9300/include
-BOARD_RIL_CLASS := ../../../device/samsung/i9300/include
+BOARD_RIL_CLASS := ../../../hardware/samsung/ril
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9300/bluetooth
