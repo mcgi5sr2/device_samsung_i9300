b74ae0acaa03a97acec50e5fe0400fd3a6bdb730
mcgi5sr2 <mcgi5sr2@gmail.com>
2015-12-10 09:40:31 +0000

    i9300: RIL use dimitry RIL workaround
    Conflicts:
    	system.prop
    
    Change-Id: I440e2f862fca34df274975e7baab941ef91e3fa2

--- i9300.mk
+++ i9300.mk
@@ -62,6 +62,11 @@ PRODUCT_PACKAGES += \
     Nfc \
     Tag
 
+# RIL - Dimitry work around
+PRODUCT_PACKAGES += \
+	dmitry-ril \
+	libdmitrygr_helper
+
 PRODUCT_COPY_FILES += \
     packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
